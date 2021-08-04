34a9b17207dd5d60190d9af834e9e758a0f4f658
secuni <anonymous>
2021-08-05 00:52:25 +0900

    dummy

--- test/create.js
+++ test/create.js
@@ -1,17 +1,22 @@
-import {check_strength, load_pw_name, do_query, parse, prove_new, do_create, return_failure, PMGet, PMPut} from "../library.js";
+import {check_strength, load_pw_name, do_query,
+    parse, prove_new,return_failure, get_prover, 
+    get_prid, prove_test, do_change, do_create, PMPut, PMGet} from "../library.js";
+
 let opener = null;
 let url_app = null;
 let step = 1;
+let prev_pwname = null;
+let pw = null;
 let pw_n = null;
 let pw_confirm = null;
-let eml = null;
+let createmode = false;
 
 document.getElementById('redo').onclick = reset_dom;
-document.getElementById('compute').onclick = await set_create_button(null, null, null, null, null, null, null);
+document.getElementById('compute').onclick = set_change_button(null, null, null, null, null, null, null, null);
 
 
 window.onload = function() {
-alert("onload");
+alsert("onload");
     // window.onblur = function(){ window.close(); };
     load_pw_name();
     window.addEventListener("message", receive_message, false);
@@ -20,23 +25,30 @@ alert("onload");
 
 
 // window.onload = async function() {
+//     var qr = new QRious({
+//         element: document.getElementById('qr'),
+//         value: 'asdfsaf'
+//     });
 //     await test()
 // }
-
 // async function test() {
-//     const dom_app = "sflab.snu.ac.kr"
-//     let id= "newtesting1234";
-//     let url_query = "https://sflab.snu.ac.kr:92?query=create&id=asdfasd";
+//     const dom_app = "sflab.snu.ac.kr";
+//     let id= "Newtest19";
+//     let url_query = "https://sflab.snu.ac.kr:92";
 //     document.getElementById("user_id").value = id;
 //     document.getElementById("url_query").value = new URL(url_query).origin;
-//     let result = await QueryCreate(id, url_query);
-//     let ty= result['ty']; let pt = result['pt']; let pt_n = result['pt_n']; let ds = result['ds_n']; let aux = result['aux'];
-//     let data = parse(pt_n)(ds)
-//     let etc = aux +';' + dom_app
-//     // set_button(pt, id, url_query, data, kh);
-//     let pw = 'asdf1234!@#$';
+//     let result = await QueryChange(id, url_query);
+//     let pt = result['pt']; let ds = result['ds']; let pt_n = result['pt_n']; let ds_n = result['ds_n'];  let kh = result['kh'];
+//     let data = parse(pt)(ds)
+//     let data_n = parse(pt_n)(ds_n);
+//     let kd = kh +';' + dom_app;
+//     let pw = 'asdf';
+//     let pw_n = 'asdf1234!@#$';
 //     try {
-//         console.log(do_create(ty, pt, pt_n, data, etc, pw))
+//         let pr = get_prover(pt)(data, pw);
+//         let [r_n, prid_n, pr_n] = prove_new(pt_n)(data_n, pw_n, kd);
+//         let ret = prove_test(pt)(data, pr, r_n);
+//         console.log(ret);
 //     } catch(err) {
 //         console.log(err)
 //     }
@@ -49,92 +61,134 @@ async function receive_message(event) {
     let parsed = event.data.split(';',2);
     let url_query = decodeURIComponent(parsed[0]);
     let id = decodeURIComponent(parsed[1]);
+    if(id==""){
+        return_failure("Insert your ID")
+    }
     document.getElementById("user_info1").value = id;
     document.getElementById("url_query").value = new URL(url_query).origin;
     window.removeEventListener("message", receive_message);
-    let result = await QueryCreate(id, url_query);
-    let aux = result['aux']; let pt = result['pt']; let pt_n = result['pt_n']; let ds_n = result['ds_n'];
-    let data_n = parse(pt_n)(ds_n)
-    let [_0, _1, ma, al] = PMGet(null, null, null);
+    let result = await QueryChange(id, url_query);
+    let aux= result['aux']; let pt = result['pt']; let ds = result['ds']; let pt_n = result['pt_n']; let ds_n = result['ds_n']; 
+    let data = ds ? parse(pt)(ds) : null;
+    let data_n = parse(pt_n)(ds_n);
+    let [pwname, {}, ma, al] = PMGet(url_query, id, get_prid(pt)(data), true);
+    prev_pwname = pwname
     document.getElementById('remember_sva').checked = ma;
     document.getElementById('remember_svp').checked = al;
+    
+    if(data === null) {
+        document.getElementById('input_name').innerHTML = "Enter a new password"
+        document.getElementById('input_value').innerHTML = '<input type="password" placeholder="Minimum 15 characters" id="user_info3" autocomplete="current-password" onkeypress="enter_pwd()">'
+        document.getElementById('prev_id').innerHTML = ""
+        document.getElementById('pw_name').autocomplete = "username"
+        document.getElementById('user_info3').focus();
+        step = 2
+        createmode = true;
+    }
+    else {
+        document.getElementById("dummy_id").value = pwname;
+        document.getElementById("user_info2").placeholder = "PWN: " +  pwname;
+    }
     document.getElementById('redo').onclick = reset_dom;
-    document.getElementById('compute').onclick = await set_create_button(id, url_query, aux, pt, pt_n, data_n, dom_app);
+    document.getElementById('compute').onclick = set_change_button(id, url_query, aux, pt, pt_n, data, data_n, dom_app);
 }
 
-async function QueryCreate(id, url_query) {
-    let url = url_query + '?query=create&id=' + encodeURIComponent(id);
+async function QueryChange(id, url_query) {
+    let url = url_query + '?query=change&id=' + encodeURIComponent(id);
     return await do_query(url);
 }
 
-function set_create_button(id, url_query, aux, pt, pt_n, data_n, dom_app) {
+function set_change_button(id, url_query, aux, pt, pt_n, data, data_n, dom_app) {
     return (async () => {
         if(step === 1) {
+            pw = document.getElementById('user_info2').value;
+            document.getElementById('input_name').innerHTML = "Enter a new password"
+            document.getElementById('input_value').innerHTML = '<input type="password" placeholder="Minimum 15 characters" id="user_info3" autocomplete="current-password" onkeypress="enter_pwd()">'
+            document.getElementById('prev_id').innerHTML = ""
+            document.getElementById('pw_name').autocomplete = "username"
+            document.getElementById('user_info3').focus();
+            step = 2
+        }
+        else if(step === 2) {
             pw_n = document.getElementById('user_info3').value;
             document.getElementById('input_name').innerHTML = "Confirm the new password"
             document.getElementById('input_value').innerHTML = '<input type="password" autofocus id="user_info4" placeholder="Minimum 15 characters" autocomplete="current-password" onkeypress="enter_pwd()">'
             document.getElementById('user_info4').focus();
-            step = 2
+            step = 3
         }
-        else if(step === 2 ){
+        else if(step === 3) {
             pw_confirm = document.getElementById('user_info4').value;
-            document.getElementById('input_name').innerHTML = '<span class="eml_info">Recovery Email &#x1F6C8;</span>'
+            document.getElementById('input_name').innerHTML = '<span class="eml_info">Recovery Email <sup>&#x1F6C8;</sup></span>'
             document.getElementById('input_value').innerHTML = '<input type="text" id="eml" autocomplete="off" onkeypress="enter_pwd()">'
             document.getElementById('compute').value = "Complete"
             document.getElementById('eml').focus();
-            step = 3
+            step = 4
         }
         else {
-            let [pw, pwn_n, eml, ma,al] = get_userpw();
-            if(pw === null) {
-                reset_dom();
+            let [pw, pw_n, pwn_n, eml, ma, al] = get_userpw();
+            let etc = dom_app + ';' + eml;
+            if((data && !(pw && pw_n)) || (!data && !pw_n)) 
                 return;
-            }
             try {
+                let ret =  null;
+                let prid = null;
+                let pr = null;
+                if(data === null) [ret, prid, pr] = await do_create(aux, pt, pt_n, data_n, etc, pw_n)
+                else [ret, prid, pr] = await do_change(aux, pt, data, pt_n, data_n, etc, pw, pw_n)
                 // let pw_name = document.getElementById("pw_name").value;
-                // PMCreate(id, url_query, pw_name);
-                let etc = dom_app + ';' + eml;
-                let [ret, prid, pr] = await do_create(aux, pt, pt_n, data_n, etc, pw)
+                // PMChange(id, url_query, pw_name, null, null);
                 opener.postMessage(ret, url_app);
                 PMPut(url_query, id, pwn_n, prid, pr, ma, al, true);
                 window.close();
             } catch(err) {
                 // this event shouldn't occur
                 console.log(err)
-                return_failure(err);
-
+                // return_failure("Error during encryption")
             }
         }
     });
 }
 
 function reset_dom() {
-    document.getElementById('input_name').innerHTML = "Enter a new password"
-    document.getElementById('input_value').innerHTML = '<input type="password" autofocus id="user_info3" placeholder="Minimum 15 characters" autocomplete="current-password" onkeypress="enter_pwd()">'
-    document.getElementById('user_info3').focus();
-    step = 1;
+
+    pw= null;
     pw_n = null;
     pw_confirm = null;
-    eml = null;
+    if(!createmode) {
+        document.getElementById('input_name').innerHTML = "Enter the current password"
+        document.getElementById('input_value').innerHTML = '<input type="password" autofocus id="user_info2" placeholder="Minimum 15 characters" autocomplete="current-password" onkeypress="enter_pwd()">'
+        document.getElementById('prev_id').innerHTML = '<input value="' + prev_pwname +'" type="text" name="dummy_id" id="dummy_id" autocomplete="username">'
+        document.getElementById('pw_name').autocomplete = ""
+        document.getElementById('user_info2').focus();
+        step = 1;
+    }
+    else {
+        document.getElementById('input_name').innerHTML = "Enter a new password"
+        document.getElementById('input_value').innerHTML = '<input type="password" placeholder="Minimum 15 characters" id="user_info3" autocomplete="current-password" onkeypress="enter_pwd()">'
+        document.getElementById('prev_id').innerHTML = ""
+        document.getElementById('pw_name').autocomplete = "username"
+        document.getElementById('user_info3').focus();
+        step = 2;
+    }
 }
 
 function get_userpw() {
     if(pw_n !== pw_confirm) { 
-        alert("The password confirmation does not match"); 
+        alert("The password confirmation does not match");
         reset_dom();
-        return [null,null]; 
+        return [null, null, null,null, null, null]; 
     }
     let strength = check_strength(pw_n);
     if(strength !== null) {
         alert(strength);
         reset_dom();
-        return [null,null];
+        return [null, null, null,null, null, null];
     }
-    let pwname = document.getElementById('pw_name').value;
+
+    let pwname = document.getElementById('pw_name') ? document.getElementById('pw_name').value : "Default";
     pwname = (pwname ? pwname : "Default");
     let eml = document.getElementById('eml').value;
-    let sva = document.getElementById("remember_sva").checked
-    let svp = document.getElementById("remember_svp").checked
-    return [pw_n, pwname, eml, sva, svp];
+    let sva = document.getElementById("remember_sva").checked   
+    let svp = document.getElementById("remember_svp").checked   
+    return [pw, pw_n, pwname, eml, sva, svp];
 }
-
